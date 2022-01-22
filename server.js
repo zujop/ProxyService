e069c08baf71b67bbb97bbdfcb3c12ab7e2c4777
Zujop <39177365+zujop@users.noreply.github.com>
2022-01-22 17:00:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,38 @@ const ALLOWED_PROTOS = ['http', 'https'];
 const ALLOWED_GZIP_METHODS = ['transform', 'decode', 'append'];
 const DEFAULT_PROTO = 'https';
 const DEFAULT_USERAGENT = 'Mozilla';
+const { Client, Intents } = require('discord.js');
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, ]});
+const {readdirSync} = require('fs')
+client.commands = new Map()
+
+
+const commands = readdirSync('./Commands').filter((file) => 
+    file.endsWith('.js')
+)
+
+
+for (command of commands) {
+    const file = require(`./Commands/${command}`)
+    client.commands.set(file.name.toLowerCase(), file)
+}
+
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("message", (message) => {
+  if (message.author.bot) return
+    if (!message.guild) return
+    const prefix = '-'
+    const args = message.content.slice(prefix.length).split(/ +/g)
+    const command = args.shift()
+
+    if (!client.commands.has(command)) return
+    client.commands.get(command).execute(client,message,args)
+})
+
 
 const getHosts = (hosts) => {
   if (!hosts) {
@@ -281,7 +313,7 @@ server.on('request', (req, res) => {
     writeErr(res, 400, 'proxy-access-key and proxy-target headers are both required');
   }
 });
-
+client.login(process.env.TOKEN)
 server.listen(PORT, (err) => {
   if (err) {
     console.error(`Server listening error: ${err}`);
